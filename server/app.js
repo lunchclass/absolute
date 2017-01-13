c86f5639c01595ddfc0cc4e8ce937017dc2800a6
CoconutPerm <coconutperm@gmail.com>
2017-01-13 22:17:49 +0900

    Fix server exception issue. (#27)
    It occur server internal exception when user connect server due to not
    included render module on our project.
    So I remove line for rendering.

--- server/app.js
+++ server/app.js
@@ -29,9 +29,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
