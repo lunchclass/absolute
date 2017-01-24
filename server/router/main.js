e7fb129cb5595a1b68cb55321f9282330aa3a5b5
Jaechul Lee <arcyze@gmail.com>
2017-01-24 23:14:27 +0900

    Add router and register/unregister client api for push notification router : server/router/main.js

---
+++ server/router/main.js
@@ -0,0 +1,27 @@
+module.exports = function(app)
+{
+  app.post('/api/client',function(req,res){
+    // Add new client
+    const clientToken = req.headers['authorization'];
+    if(clientToken) {
+      console.log('Store client token : ' + clientToken);
+      // StoreClient token to DB
+      res.sendStatus(200);
+    }
+    else {
+      res.sendStatus(400); // token not found
+    }
+  });
+
+  app.delete('/api/client', function(req,res) {
+    // Remove client
+    const clientToken = req.headers['authorization'];
+    if(clientToken) {
+      console.log('Remove client token : ' + clientToken);
+      // Remove token from DB
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(400);
+    }
+  });
+}
--- server/server.js
+++ server/server.js
@@ -10,7 +10,13 @@ const httpsServer = require('./https_server/https_server');
 const redirectServer = require('./https_server/redirect_server');
 
 var app = express();
+var bodyParser = require('body-parser');
+var router = require('./router/main.js')(app); // router must be after body-parser
+
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.urlencoded({
+  extended: true}));
+app.use(bodyParser.json());
 
 httpsServer.run(app, config.serverInfo);
 redirectServer.runForHttps(config.serverInfo);
