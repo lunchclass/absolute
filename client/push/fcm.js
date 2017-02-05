94933b7198c79da8ca6fc6ba4e5b0ae9c214c6a9
daehyunjimmy <jimmysung201@gmail.com>
2017-02-05 22:06:00 +0900

    Request permission to create FCM token and send it to server
    Control firebase token

--- client/push/fcm.js
+++ client/push/fcm.js
@@ -4,20 +4,69 @@
 })(typeof window !== 'undefined' ? window : this, function(w, d) {
   var fcm = function() {
     var self = this;
+	var authorizationKey = '';
+	var clientToken = '';
+
+    var config = {
+      apiKey: '',
+      authDomain: '',
+      databaseURL: '',
+      storageBucket: '',
+      messagingSenderId: ''
+    };
+
+    firebase.initializeApp(config);
+    const messaging = firebase.messaging();
 
     function createFirebaseToken() {
+      messaging.getToken()
+      .then(function(token) {
+        if (token) {
+		  clientToken = token;
+		  sendFCMTokenToServer();
+        } else {
+          console.log('No token available.');
+        }
+      })
+      .catch(function(err) {
+        console.log('An error occurred while retrieving token. err : ', err);
+      });
     }
 
     function deleteFirebaseToken() {
     }
 
     function requestPushPermission() {
+      messaging.requestPermission()
+      .then(function() {
+        createFirebaseToken();
+      })
+      .catch(function(err) {
+        console.log('Notification permission denied. err : ', err);
+      });
+    }
+
+    function sendFCMTokenToServer() {
+      var http = new XMLHttpRequest();
+      var url = "https://localhost:9443/api/client";
+      http.open("POST", url, true);
+      http.setRequestHeader("Content-Type", "application/json");
+      http.setRequestHeader("Authorization", "key=",authorizationKey);
+      http.onreadystatechange = function() {
+        if (http.readyState == XMLHttpRequest.DONE && http.status == 200) {
+          console.log(http.responseText);
+        }
+      }
+      http.send(JSON.stringify({
+        to: clientToken
+      }));
     }
 
     /**
      * Register firebase push
      */
     self.register = function() {
+      requestPushPermission();
     };
 
     /**
