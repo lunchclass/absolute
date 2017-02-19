076e1af59af47470204413fba11608d875c2b602
Jaechul Lee <arcyze@gmail.com>
2017-02-19 23:33:45 +0900

    Add PushManager and Notification for Server Push - Add router dir and main.js for apis - Add PushManager and send-push-notification into push dir - Add body-parser, request npm package for push

---
+++ server/push/push-manager.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2017 The Absolute Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+exports.addClient = function (clientToken) {
+  // save client token
+  console.log('add client token :' + clientToken);
+};
+
+exports.removeClient = function (clientToken) {
+  // remove client token
+  console.log('remove client token :' + clientToken);
+};
+
+exports.updateToken = function (oldClientToken, newClientToken) {
+  // removeClient(oldClientToken);
+  // addclient(newClientToken);
+};
+
+exports.sendPushNotification = function (clientToken) {
+  const push = require('./send-push-notification.js');
+  //push.sendFCMNotification(clientToken);
+  setTimeout(push.sendFCMNotification, 2000, clientToken);
+  console.log('send push!');
+};
---
+++ server/push/send-push-notification.js
@@ -0,0 +1,32 @@
+// Copyright (c) 2017 The Absolute Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+var serverKey = 'Your server key here, Never expose to client code';
+
+exports.sendFCMNotification = function (clientToken) {
+  console.log("Send push notification");
+  const request = require('request');
+  const jsonHeader = {
+    "Authorization" : "key=" + serverKey,
+    "Content-Type" : "application/json"
+  };
+
+  const jsonBody = {
+    "notification": {
+    "title": "Absolute FCM Push Notification",
+    "body": "Sent from absolute!",
+    "icon": "firebase-icon.png",
+    "click_action": "https://localhost:9443/"
+    },
+    "to": clientToken
+  };
+
+  request({
+    url : 'https://fcm.googleapis.com/fcm/send',
+    method : "POST",
+    json : true,
+    headers : jsonHeader,
+    body : jsonBody
+  }, function (error, response, body){
+  });
+};
