c6de7e84f7a4ae3033fe2b45ae984933bda03d05
Jinho Bang <romandev@users.noreply.github.com>
2017-02-24 01:32:56 +0900

    Remove legacy run.cmd and scripts. (#81)
    The codes are replaced to new bootstrap module. (#73)

--- run.cmd
+++
@@ -1,6 +0,0 @@
-:; # Bootstrap code for Mac/Linux
-:; cd $(dirname $0) && ./scripts/run $@; exit $?
-
-:; # Bootstrap code for Windows
-@ECHO OFF
-cd %~dp0 && third_party\win-bash\bash scripts\run %*
