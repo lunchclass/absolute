lunchclass/absolute

commit bc5fa71b3a19ee6829821387b3726667375d88cc

Implement a new bootstrap scripts. (#73)
This CL includes the follwing things.
 - Improve existing scripts.
 - Introduce |absolute| command for all platforms.
 - After this patch, we can add a new subcommand easily in bootstrap/command/.
 - Introduce very simple platform test. The test will be more improved later.
 - Fix a bug that executable files are located in server/ on windows (#69).
 - Remove unnecesary script parts in package.json
 - Stop to use nodemon until fixing it (#71)

absolute.cmd
@@ -1,0 +1,6 @@
+; Copyright (c) 2017 The Absolute Authors. All rights reserved.
+; Use of this source code is governed by a BSD-style license that can be
+; found in the LICENSE file.
+
+@ECHO OFF
+cd %~dp0 && third_party\win-bash\bash bootstrap\absolute.sh %*

bootstrap/common/path_info.sh
@@ -1,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright (c) 2017 The Absolute Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+function absolute_path() {
+  echo $(cd $(dirname $BASH_SOURCE)/../..; pwd)
+}
+
+function third_party_path() {
+  echo $(absolute_path)/third_party
+}
+
+function bootstrap_path() {
+  echo $(absolute_path)/bootstrap
+}
+
+function bootstrap_command_path() {
+  echo $(bootstrap_path)/command
+}
+
+function server_path() {
+  echo $(absolute_path)/server
+}
+
+function set_path_env() {
+  export PATH="$1":"$PATH"
+}

.gitignore
@@ -1,6 +1,4 @@
-node_modules
-platform
 *.swp
-*.tar.gz
-*.zip
-npm-debug.log
+node_modules
+third_party/mongodb
+third_party/node

.travis.yml
@@ -1,9 +1,3 @@
 language: node_js
-node_js: 
-  - "node"
-install:
-  - npm install -g mocha
-env:
-  - TEST_DIR=server
-  - TEST_DIR=client
-script: cd $TEST_DIR && mocha ./test/*.js
+script:
+  - ./absolute platform_test

absolute
@@ -1,0 +1,8 @@
+#!/bin/bash
+#
+# Copyright (c) 2017 The Absolute Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+absolute_path=$(cd $(dirname $BASH_SOURCE); pwd)
+cd $absolute_path && source ./bootstrap/absolute.sh $@;

bootstrap/absolute.sh
@@ -1,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright (c) 2017 The Absolute Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+. bootstrap/common/path_info.sh
+. bootstrap/common/sync_third_party.sh
+
+# Sync third_parties.
+sync_node
+sync_mongodb
+
+# Set path
+set_path_env $(third_party_path)/node/bin
+set_path_env $(third_party_path)/mongodb/bin
+set_path_env $(bootstrap_command_path)
+set_path_env $(server_path)/node_modules/.bin
+
+for command in $(ls $(bootstrap_command_path)); do
+  if [ "$1" = "$command" ]; then
+    shift
+    $(bootstrap_command_path)/$command $@
+    return
+  fi
+done
+
+$(bootstrap_command_path)/help

bootstrap/command/help
@@ -1,0 +1,12 @@
+#!/bin/bash
+#
+# Copyright (c) 2017 The Absolute Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+. bootstrap/common/path_info.sh
+
+echo "Available commands:"
+for command in $(ls $(bootstrap_command_path)); do
+  echo "    absolute $command"
+done

bootstrap/command/platform_test
@@ -1,0 +1,9 @@
+#!/bin/bash
+#
+# Copyright (c) 2017 The Absolute Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+. bootstrap/test/platform_test.sh
+
+test_sync_node

commit 30dff0b6e015ce55f8c4c71613609a504ff7a38b

Fix a bug when absolute runs on windows.
This is a regression after #73.

absolute.cmd
@@ -1,6 +1,6 @@
-; Copyright (c) 2017 The Absolute Authors. All rights reserved.
-; Use of this source code is governed by a BSD-style license that can be
-; found in the LICENSE file.
+:: Copyright (c) 2017 The Absolute Authors. All rights reserved.
+:: Use of this source code is governed by a BSD-style license that can be
+:: found in the LICENSE file.
 
 @ECHO OFF
 cd %~dp0 && third_party\win-bash\bash bootstrap\absolute.sh %*

bootstrap/common/path_info.sh
@@ -5,7 +5,7 @@
 # found in the LICENSE file.
 
 function absolute_path() {
-  echo $(cd $(dirname $BASH_SOURCE)/../..; pwd)
+  pwd
 }
 
 function third_party_path() {