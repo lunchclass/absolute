d753619c1f40d72a61825ce3f654122e79c4c172
daehyunjimmy <jimmysung201@gmail.com>
2017-02-25 21:03:59 +0900

    Update firebase-messaging-sw.js

--- client/firebase-messaging-sw.js
+++ client/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.10/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.10/firebase-messaging.js');
 
 firebase.initializeApp({
   'messagingSenderId': ''
