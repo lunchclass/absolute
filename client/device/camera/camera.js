99249bf735e73a9ef8cdbe4b2fa24e1aba231f2c
cs-lee <changsu_lee@ymail.com>
2017-03-01 16:05:33 +0900

    Add API to control user mobile camera device.

---
+++ client/device/camera/camera.js
@@ -0,0 +1,23 @@
+(function (window) {
+  /**
+   * Set correct image orientation.
+   */
+  var resetOrientation = function (imgSrc, orientation, callback) {
+  };
+
+  /**
+   * Get orientation value from captured image.
+   */
+  var getOrientation = function (file, callback) {
+  };
+
+  /**
+   * API for control user mobile camera device.
+   */
+  var camera = {
+    setImage : function (inputId, callback) {
+    }
+  };
+
+  window.camera = camera;
+})(window);
