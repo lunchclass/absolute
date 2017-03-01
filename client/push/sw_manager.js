99f44bb7eecc02a5c1d86094a575901b0edd123a
daehyunjimmy <jimmysung201@gmail.com>
2017-03-01 15:17:00 +0900

    Add implementation of register call
    * implementation of register API
    - register Service Worker
    - subscribe push manager
    - send push description to server

--- client/push/sw_manager.js
+++ client/push/sw_manager.js
@@ -8,6 +8,29 @@
     var swRegistration = '';
 
     function registerServiceWorker() {
+      if ('serviceWorker' in navigator && 'PushManager' in window) {
+        navigator.serviceWorker.register('sw.js')
+        .then(function(registration) {
+          swRegistration = registration;
+          navigator.serviceWorker.ready.then(function(registration) {
+            serviceWorkerRegistration.pushManager.subscribe({
+              userVisibleOnly: true
+            })
+            .then(function(subscription) {
+              pushSubscription = subscription;
+              sendPushTokenToServer();
+            })
+            .catch(function(err) {
+              console.log('subscribe error ', err);
+            });
+          });
+        })
+        .catch(function(error) {
+          console.error('service worker register error ', error);
+        });
+      } else {
+        console.warn('push is not supported');
+      }
     }
 
     function unregisterServiceWorker() {
@@ -17,6 +40,18 @@
     }
 
     function sendPushTokenToServer() {
+      var http = new XMLHttpRequest();
+      var url = "https://localhost:9443/api/push/client";
+      http.open("POST", url, true);
+      http.setRequestHeader("Content-Type", "application/json");
+      http.onreadystatechange = function() {
+        if (http.readyState == XMLHttpRequest.DONE && http.status == 200) {
+          console.log(http.responseText);
+        }
+      }
+      http.send(JSON.stringify({
+        token: pushSubscription
+      }));
     }
 
     function requestPushPermission() {
@@ -29,6 +64,7 @@
      * Register firebase push
      */
     self.register = function() {
+      registerServiceWorker();
     };
 
     /**
