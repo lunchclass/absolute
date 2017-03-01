87084a93078eb6c45e6c3030ab2ba6ef66c7fc68
CoconutPerm <coconutperm@gmail.com>
2017-03-01 17:02:39 +0900

    Add rest api for order menu.
    We need rest api for order menu.
    https://{address}/api/order (POST)

---
+++ server/order/router/order_router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const Order = require('../model/order');
+
+const router = express.Router();
+
+router.post('/', function (req) {
+  const order = new Order(JSON.parse(JSON.stringify(req.body)));
+  order.save();
+});
+
+module.exports = router;
