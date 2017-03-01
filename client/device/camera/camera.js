1a9b1e388bd41d909b4f4218b33e5743d304cfc0
cs-lee <changsu_lee@ymail.com>
2017-03-01 20:30:34 +0900

    Implemention of resetOrientation function. This function set correction orientation. Change camera API function name. Issue number #105

--- client/device/camera/camera.js
+++ client/device/camera/camera.js
@@ -4,11 +4,11 @@
    */
   var resetOrientation = function (imgSrc, orientation, callback) {
     var img = new Image();
-    img.onload = function() {
-      var width = img.width,
-          height = img.height,
-          canvas = document.createElement('canvas'),
-          ctx = canvas.getContext("2d");
+    img.onload = function () {
+      var width = img.width;
+      var height = img.height;
+      var canvas = document.createElement('canvas');
+      var ctx = canvas.getContext('2d');
       if (orientation >= 5 && orientation <= 8) {
         canvas.width = height;
         canvas.height = width;
@@ -17,14 +17,14 @@
         canvas.height = height;
       }
       switch (orientation) {
-        case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
-        case 3: ctx.transform(-1, 0, 0, -1, width, height ); break;
-        case 4: ctx.transform(1, 0, 0, -1, 0, height ); break;
-        case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
-        case 6: ctx.transform(0, 1, -1, 0, height , 0); break;
-        case 7: ctx.transform(0, -1, -1, 0, height , width); break;
-        case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
-        default: ctx.transform(1, 0, 0, 1, 0, 0);
+      case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+      case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
+      case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
+      case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+      case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
+      case 7: ctx.transform(0, -1, -1, 0, height, width); break;
+      case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+      default: ctx.transform(1, 0, 0, 1, 0, 0);
       }
       ctx.drawImage(img, 0, 0);
       callback(canvas.toDataURL());
@@ -42,8 +42,12 @@
    * API for control user mobile camera device.
    */
   var camera = {
-    getImageFromCamera : function (callback) {
-    }
+    /**
+     * Get image from camera.
+     * This function should be called from user action.
+     */
+    getImageFromCamera(callback) {
+    },
   };
 
   window.camera = camera;
