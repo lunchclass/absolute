761fe10151ad43ad518dde595d35c5ee5114523f
Byoungkwon Ko <gogag2@gmail.com>
2017-03-01 20:19:07 +0900

    Implement deep link to open an external browser. KakaoTalk is using internal WebView to open link. But we can't hand service worker and related features. So, when we open link in kakao talk, we need to open link using chrome or sbrowser.
    BUG=#96

--- client/deeplink/deeplink.js
+++ client/deeplink/deeplink.js
@@ -1,15 +1,16 @@
-(function () {
+(function (window) {
   const deeplink = {
     createBrowser(appPackage) {
       const intentURI = [
         'intent://www.nadongguri.com#Intent',
         'scheme=https',
         `package=${appPackage}`,
-        'S.browser_fallback_url=https://www.nadongguri.com',
+        'S.browser_fallback_url=http://www.nadongguri.com/wedding',
         'end',
       ].join(';');
 
       window.location.href = intentURI;
     },
   };
-}());
+  window.deeplink = deeplink;
+}(window));
