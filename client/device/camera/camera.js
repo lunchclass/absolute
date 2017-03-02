a2bc9a8ddf2c49369eb0a3b4aeb9765c975d450e
cs-lee <changsu_lee@ymail.com>
2017-03-02 23:58:52 +0900

    Implementation of getOrientation function. This function get orientation value from image. Issue#105

--- client/device/camera/camera.js
+++ client/device/camera/camera.js
@@ -9,6 +9,7 @@
       var height = img.height;
       var canvas = document.createElement('canvas');
       var ctx = canvas.getContext('2d');
+
       if (orientation >= 5 && orientation <= 8) {
         canvas.width = height;
         canvas.height = width;
@@ -16,6 +17,7 @@
         canvas.width = width;
         canvas.height = height;
       }
+
       switch (orientation) {
       case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
       case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
@@ -26,6 +28,7 @@
       case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
       default: ctx.transform(1, 0, 0, 1, 0, 0);
       }
+
       ctx.drawImage(img, 0, 0);
       callback(canvas.toDataURL());
     };
@@ -36,6 +39,51 @@
    * Get orientation value from captured image.
    */
   var getOrientation = function (file, callback) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      var view = new DataView(e.target.result);
+      var length = null;
+      var offset = null;
+      var marker = null;
+      var little = null;
+      var tags = null;
+      var i = null;
+
+      if (view.getUint16(0, false) !== 0xFFD8) {
+        return callback(-2);
+      }
+      length = view.byteLength;
+      offset = 2;
+      while (offset < length) {
+        marker = view.getUint16(offset, false);
+        offset += 2;
+        if (marker === 0xFFE1) {
+          offset += 2;
+          if (view.getUint32(offset, false) !== 0x45786966) {
+            return callback(-1);
+          }
+
+          little = view.getUint16(offset += 6, false) === 0x4949;
+          offset += view.getUint32(offset + 4, little);
+          tags = view.getUint16(offset, little);
+          offset += 2;
+
+          for (i = 0; i < tags; i += 1) {
+            if (view.getUint16(offset + (i * 12), little) === 0x0112) {
+              return callback(view.getUint16(offset + (i * 12) + 8, little));
+            }
+          }
+        /*eslint-disable */
+        } else if ((marker & 0xFF00) !== 0xFF00) {
+        /*eslint-enable */
+          break;
+        } else {
+          offset += view.getUint16(offset, false);
+        }
+      }
+      return callback(-1);
+    };
+    reader.readAsArrayBuffer(file);
   };
 
   /**
