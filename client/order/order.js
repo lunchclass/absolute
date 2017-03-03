0ef078359e7fd6d2bb8f8989391d4c310e6cd24d
michael <sunemo83@samsung.com>
2017-03-03 10:28:12 +0900

    Testing order API

--- client/order/order.js
+++ client/order/order.js
@@ -1,27 +1,82 @@
 (function (global, factory) {
-  /**
-   * Set correct image orientation.
-   */
+  'use strict';	
   /*eslint-disable */
-  global.order = new factory(global, global.document)();
+  global.ordering = new factory(global, global.document)();
   /*eslint-enable */
 }(typeof window !== 'undefined' ? window : this, function (w, d) {
-  var order = function () {
-    var self = this;
-    /**
-     * Not implementation
-     */
-    function sendOrder() {
-      /**
-       * Not implementation
-       */
+  var ordering = function () {
+	var self = this;
+    console.log("debug: order-api");
+	
+	function initInfo() {
+		var content = {
+            data: "",
+			contentType: ""		
+        };
+		
+		var itemList = [{
+            name: "",
+            count: "",
+            amount: "",
+        }];
+		
+		var info = {
+            userId: "",
+            items: itemList,
+            totalAmount: "",
+            image: content,
+            orderDate: "",
+            options: "",
+        }		
+		
+		return info;
+	}
+	
+	function readContent(dataUrl, done) {
+		var reader  = new FileReader();
+		reader.readAsDataURL(dataUrl);
+		
+		reader.onload = function(e) {
+            var content = e.target.result;
+			//FIXME:
+		    var contentType = "image/jpeg";
+            console.log(content);
+			done(content, contentType);
+        };
+	}
+	
+	function o(data, contentType)
+	
+    function sendOrder(dataUrl) {
+        readData(dataUrl, );
+		
+	    var json = JSON.stringify(orderInfo);
+	    var host = document.location.hostname;
+		var port = document.location.port;
+		var apiUrl="/api/order";
+		var targetUrl = "//" + host + ":" + port + apiUrl;
+		console.log(targetUrl);
+        var xhr = new XMLHttpRequest();
+	    xhr.open('post', targetUrl);
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.send(json);
+		 
+		xhr.onreadystatechange = function() {//Call a function when the state changes.
+            if(xhr.readyState == XMLHttpRequest.DONE) {
+			    if (xhr.status == 400) {
+                    console.log("debug: wtf");
+			    } else if (xhr.status == 200) {
+					console.log("debug: 200 ok");
+				}
+			}	
+		}	
     }
+	
     self.order = function (json) {
-      /**
-       * Not implementation
-       */
-      sendOrder(json);
+      console.log('debug. called this func');
+      sendOrder(dataUrl);
     };
+	
   };
-  return order;
+  return ordering;
 }));
