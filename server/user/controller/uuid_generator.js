893ac9aa061093f490de6ad57b9bc30381a31f39
CoconutPerm <coconutperm@gmail.com>
2017-03-04 14:28:03 +0900

    Implement uuid generator rest api
    We need unique key for distributed system.
    So implement rest api for generate uuid.

---
+++ server/user/controller/uuid_generator.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2017 The Absolute Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const uuid = require('node-uuid');
+
+exports.generateUuid = function generateUuid() {
+  return { uuid: uuid.v4() };
+};
