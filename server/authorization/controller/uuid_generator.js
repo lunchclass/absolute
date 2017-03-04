d0af71ece76e47426d931de67535c0fb94eb390e
CoconutPerm <coconutperm@gmail.com>
2017-03-04 14:46:00 +0900

    Change pkg
    change pkg

--- server/authorization/controller/uuid_generator.js
+++ server/authorization/controller/uuid_generator.js
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-const uuid = require('node-uuid');
+const uuid = require('uuid/v4');
 
 exports.generateUuid = function generateUuid() {
-  return { uuid: uuid.v4() };
+  return { uuid: uuid() };
 };
