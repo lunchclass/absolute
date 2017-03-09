b19eab37b50e6bba9e5508506be1553e06ddcee9
Byoungkwon Ko <gogag2@gmail.com>
2017-03-10 02:25:31 +0900

    Deeplink should open default browser without chooser activity. And Chrome and SamsungBrowser don't need to run deeplink.
    BUG = #94

--- client/deeplink/deeplink.js
+++ client/deeplink/deeplink.js
@@ -1,14 +1,41 @@
 (function (window) {
-  const deeplink = {
-    createBrowser(appPackage) {
-      const intentURI = [
-        'intent://www.nadongguri.com/wedding#Intent',
-        'scheme=https',
-        'S.browser_fallback_url=http://www.nadongguri.com/wedding',
-        'end',
-      ].join(';');
+  function sendDeepLink() {
+    const intentURI = [
+      'intent://nadongguri.com/wedding#Intent',
+      'scheme=https',
+      'S.browser_fallback_url=http://nadongguri.com/wedding',
+      'end',
+    ].join(';');
 
-      window.location.href = intentURI;
+    window.location.href = intentURI;
+  }
+  function isLatestWebView() {
+    return /Version\/[.0-9]* Chrome\/[.0-9]*/.test(navigator.userAgent);
+  }
+  function isAndroidDevice() {
+    return /Android/.test(navigator.userAgent);
+  }
+  function isSamsungBrowser() {
+    return /SamsungBrowser/.test(navigator.userAgent);
+  }
+  function isChrome() {
+    return /Chrome/.test(navigator.userAgent);
+  }
+  function goToWeddingSite() {
+    window.location.href = 'http://nadongguri.com/wedding';
+  }
+
+  const deeplink = {
+    createBrowser() {
+      if (isAndroidDevice()) {
+        if (isLatestWebView() || (!isSamsungBrowser() && !isChrome())) {
+          sendDeepLink();
+        } else {
+          goToWeddingSite();
+        }
+      } else {
+        goToWeddingSite();
+      }
     },
   };
   window.deeplink = deeplink;
