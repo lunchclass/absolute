80dff534ad109d2c9fc1e3385c40c6f81fc1045c
hyungheo <sunemo83@gmail.com>
2017-03-10 22:58:36 +0900

    Syntax:   getUuid() Return:   Promise   then: function (uuid) Example:   getUuid().then(function(uuid) {     return uuid;   } Description:   If browser dosen't have auth-uuid in its localstorage   when this api called, This api'll requests the server to make new uuid and   save it in browser's localstorgae as 'auth-uuid'.   Also return it through promise callback function.

--- client/authorization/authorization.js
+++ client/authorization/authorization.js
@@ -1,27 +1,30 @@
 (function (global, factory) {
-  /*eslint-disable */
+/* eslint-disable */
   global.auth = new factory(global, global.document)();
-  /*eslint-enable */
+/* eslint-enable */
 }(typeof window !== 'undefined' ? window : this, function (w, d) {
   var auth = function () {
-    var self = this;
-    var AuthInfo = function (uuid, error) {
-      this.uuid = uuid;
-      this.error = error;
-    };
+    const self = this;
+
     const HOST = self.location.hostname;
     const PORT = self.location.port;
-    const API_URL = '/api/authorization/uuid/';
+    const API_URL = '/api/authorization/uuid';
     const TARGET_URL = `//${HOST}:${PORT}${API_URL}`;
     const AUTH_ERROR = {
       NONE: 0,
       GENERAL: 1,
     };
-    var requestUuid = function () {
+
+    var AuthInfo = function (uuid, error) {
+      this.uuid = uuid;
+      this.error = error;
+    };
+
+    var requestSetUuid = function () {
       return new Promise(function (resolve, reject) {
         var xhr = new XMLHttpRequest();
-        xhr.open('post', TARGET_URL);
-        xhr.setRequestHeader('Content-Type', 'application/javascript');
+        xhr.open('POST', TARGET_URL, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send();
         xhr.onreadystatechange = function () {
           var resp = null;
@@ -40,28 +43,82 @@
       });
     };
 
-    function saveUuid(authinfo) {
+    var requestGetUuid = function (localUuid) {
       return new Promise(function (resolve, reject) {
-        if (authinfo.error === AUTH_ERROR.NONE) {
-          localStorage.setItem('auth_uuid', authinfo.uuid);
-          resolve(authinfo.error);
-        } else {
-          reject(authinfo.error);
+        var xhr = new XMLHttpRequest();
+        var queryUrl = `${TARGET_URL}?uuid=${localUuid}`;
+        var authInfo = null;
+        xhr.open('GET', queryUrl, true);
+        try {
+          xhr.send();
+        } catch (error) {
+          authInfo = new AuthInfo('', AUTH_ERROR.GENERAL);
+          reject();
         }
-      });
-    }
-
-    // PUBLIC API
-    self.createUuid = function () {
-      return new Promise(function (resolve, reject) {
-        requestUuid().then(function (result) { return saveUuid(result); })
-                 .then(function (e) { resolve(e); });
+        xhr.onreadystatechange = function () {
+          var resp = null;
+          if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+              resp = JSON.parse(xhr.responseText);
+              authInfo = new AuthInfo(resp.uuid, AUTH_ERROR.NONE);
+              resolve(authInfo);
+            } else {
+              authInfo = new AuthInfo('', AUTH_ERROR.GENERAL);
+              reject(authInfo);
+            }
+          }
+        };
       });
     };
 
+    var saveUuid = function (authinfo) {
+      return new Promise(function (resolve, reject) {
+        if (authinfo.error === AUTH_ERROR.NONE) {
+          localStorage.setItem('auth_uuid', authinfo.uuid);
+          resolve(authinfo);
+        } else {
+          reject(authinfo);
+        }
+      });
+    };
+
+    var isValidUuid = function (localUuid) {
+      return new Promise(function (resolve, reject) {
+        if (localUuid === '' || localUuid === null || localUuid === undefined) {
+          reject();
+        } else {
+          requestGetUuid(localUuid).then(
+          function (e) {
+            resolve(localUuid);
+          },
+          function (e) {
+            reject();
+          });
+        }
+      });
+    };
+
+    var passLocalUuid = function (uuid) {
+      var authInfo = new AuthInfo(uuid, AUTH_ERROR.NONE);
+      return Promise.resolve(authInfo);
+    };
+
+    var creatNewUuid = function () {
+      return new Promise(function (resolve, reject) {
+        requestSetUuid().then(saveUuid)
+                        .then(function (authInfo) { resolve(authInfo); });
+      });
+    };
+
+    // PUBLIC API
     self.getUuid = function () {
-      return localStorage.getItem('auth_uuid');
+      return new Promise(function (resolve, reject) {
+        var localUuid = localStorage.getItem('auth_uuid');
+        isValidUuid(localUuid).then(passLocalUuid, creatNewUuid)
+                              .then(function (authInfo) { resolve(authInfo); });
+      });
     };
   };
   return auth;
 }));
+
