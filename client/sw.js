6fe81d29fdbd7b34a9c89c4ade80d066b2707bf6
Jaechul Lee <arcyze@gmail.com>
2017-03-13 00:13:39 +0900

    Use static notification data when event.data is undefined on service worker

--- client/sw.js
+++ client/sw.js
@@ -1,17 +1,41 @@
 /* eslint-disable */
 
 self.addEventListener('push', function (event) {
-  var data = {};
+
+  var title = "";
+  var body = "";
+
   if (event.data) {
+    var data = {};
+    console.log('get push notification data');
     data = event.data.json();
+    title = data.notification.title;
+    body = data.notification.body;
+  } else {
+    console.log('use static push notification data');
+
+    title = "나동진♡김소영 결혼을 축하해주세요!"
+    const notificationOptions = {
+      body: '2017년 4월 8일 토요일 오후 5시',
+      icon: './images/main.jpg',
+      badge: './images/badge-72x72.png',
+      tag: 'wedding',
+      url: 'https://nadongguri.com/wedding/'
+    };
+
+    body = notificationOptions;
   }
 
   event.waitUntil(
     self.registration.showNotification(
-      data.notification.title, data.notification)
+      title, body)
   );
+
 });
 
 self.addEventListener('notificationclick', function (event) {
-});
+  event.notification.close();
 
+  let clickResponsePromise = Promise.resolve();
+  clickResponsePromise = clients.openWindow("https://nadongguri.com/wedding/");
+});
