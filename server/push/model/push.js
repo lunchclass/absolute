01611889d7dbf858cbc47beefaeabe9b8c549172
Jaechul Lee <arcyze@gmail.com>
2017-03-20 22:59:52 +0900

    Add set/get notification data for push - set : (POST, /notification/message/:id) - get : (GET, /notification/message/:id) If ':id' is not given, handle default notification

---
+++ server/push/model/push.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2017 The Absolute Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  userId: { type: String, lowercase: true, trim: true, unique: true },
+  token: { type: String, lowercase: true, trim: true, unique: true },
+  title: { type: String, trim: true },
+  body: { type: String, trim: true },
+  icon: { type: String, trim: true },
+  url: { type: String, trim: true },
+});
+
+module.exports = mongoose.model('push', schema);
