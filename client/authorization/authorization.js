4acb96d01237ba18b9b8201efc94ec29f51bb0cb
michael <sunemo83@samsung.com>
2017-03-21 10:27:16 +0900

    Replace localStorage to IndexedDb
    cause: serviceworker cannot use localstorage in browser

--- client/authorization/authorization.js
+++ client/authorization/authorization.js
@@ -13,7 +13,11 @@
     const AUTH_ERROR = {
       NONE: 0,
       GENERAL: 1,
+      OPEN_DB_FAIL: 2,
     };
+
+    const DB_NAME = 'absolute';
+    const INIT_DATA = { title: 'absolute', uuid: '' };
 
     var AuthInfo = function (uuid, error) {
       this.uuid = uuid;
@@ -71,11 +75,133 @@
       });
     };
 
+    var onupgradeneeded = function (event) {
+      var db = event.target.result;
+      var objectStore = db.createObjectStore('auth', { keyPath: 'title' });
+
+      // Create Index
+      objectStore.createIndex('uuid', 'uuid', { unique: true });
+
+      // Init Data
+      objectStore.transaction.oncomplete = function (e) {
+        var objstore = db.transaction('auth', 'readwrite').objectStore('auth');
+        objstore.add(INIT_DATA);
+      };
+    };
+
+    var openDb = function (dbName, updateValue) {
+      return new Promise(function (resolve, reject) {
+        var indexedDB = self.indexedDB ||
+                        self.mozIndexedDB ||
+                        self.webkitIndexedDB ||
+                        self.msIndexedDB;
+        var request;
+        if (!indexedDB) {
+          reject(AUTH_ERROR.OPEN_DB_FAIL);
+        } else {
+          request = indexedDB.open(dbName, 2);
+          // onupgradeneeded
+          request.onupgradeneeded = onupgradeneeded;
+          // onsuccess
+          request.onsuccess = function (event) {
+            var db = event.target.result;
+            resolve({ targetDb: db, source: updateValue });
+          };
+          // onerror
+          request.onerror = function (error) {
+            reject(AUTH_ERROR.OPEN_DB_FAIL);
+          };
+        }
+      });
+    };
+
+    var getIndex = function (data) {
+      return new Promise(function (resolve, reject) {
+        var db = data.targetDb;
+        var sourceData = data.source;
+        var tran = db.transaction(['auth'], 'readwrite');
+        var objectStore = tran.objectStore('auth');
+        var request = objectStore.get('absolute');
+
+        request.onsuccess = function (event) {
+          var targetData = event.target.result;
+          resolve({ targetDb: db,
+            target: targetData,
+            source: sourceData });
+        };
+
+        request.onerror = function (error) {
+          reject(error);
+        };
+      });
+    };
+
+    var setValue = function (result) {
+      return new Promise(function (resolve, reject) {
+        var db = result.targetDb;
+        var tran = db.transaction(['auth'], 'readwrite');
+        var objectStore = tran.objectStore('auth');
+        var data = result.target;
+        var request;
+        data.uuid = result.source;
+        request = objectStore.put(data);
+        // onsuccess
+        request.onsuccess = function (event) {
+          resolve(AUTH_ERROR.NONE);
+        };
+        // onerror
+        request.onerror = function (error) {
+          reject(AUTH_ERROR.OPEN_DB_FAIL);
+        };
+      });
+    };
+
+    var getValue = function (result) {
+      return new Promise(function (resolve, reject) {
+        var data = result.target;
+        if (!data) {
+          reject(AUTH_ERROR.OPEN_DB_FAIL);
+        } else {
+          resolve(data.uuid);
+        }
+      });
+    };
+
+    var handleDbError = function (error) {
+      return Promise.reject(error);
+    };
+
+
+    var saveUuidToDb = function (dbName, value) {
+      return new Promise(function (resolve, reject) {
+        openDb(dbName, value).then(getIndex, handleDbError)
+                             .then(setValue, handleDbError)
+                             .then(function (r) { resolve(r); },
+                                   function (e) { reject(e); });
+      });
+    };
+
+    var getUuidToDb = function (dbName) {
+      return new Promise(function (resolve, reject) {
+        openDb(dbName).then(getIndex, handleDbError)
+                      .then(getValue, handleDbError)
+                      .then(function (r) { resolve(r); },
+                            function (e) { reject(e); });
+      });
+    };
+
     var saveUuid = function (authinfo) {
       return new Promise(function (resolve, reject) {
         if (authinfo.error === AUTH_ERROR.NONE) {
-          localStorage.setItem('auth_uuid', authinfo.uuid);
-          resolve(authinfo);
+          saveUuidToDb(DB_NAME, authinfo.uuid).then(function (r) {
+            getUuidToDb(DB_NAME).then(function (result) {
+              var retInfo = new AuthInfo(result, AUTH_ERROR.NONE);
+              resolve(retInfo);
+            }, function (e) {
+              var retInfo = new AuthInfo('', AUTH_ERROR.OPEN_DB_FAIL);
+              reject(retInfo);
+            });
+          });
         } else {
           reject(authinfo);
         }
@@ -103,7 +229,7 @@
       return Promise.resolve(authInfo);
     };
 
-    var creatNewUuid = function () {
+    var createNewUuid = function () {
       return new Promise(function (resolve, reject) {
         requestSetUuid().then(saveUuid)
                         .then(function (authInfo) { resolve(authInfo); });
@@ -113,12 +239,11 @@
     // PUBLIC API
     self.getUuid = function () {
       return new Promise(function (resolve, reject) {
-        var localUuid = localStorage.getItem('auth_uuid');
-        isValidUuid(localUuid).then(passLocalUuid, creatNewUuid)
-                              .then(function (authInfo) { resolve(authInfo); });
+        getUuidToDb(DB_NAME).then(isValidUuid)
+                            .then(passLocalUuid, createNewUuid)
+                            .then(function (authInfo) { resolve(authInfo); });
       });
     };
   };
   return auth;
 }));
-
