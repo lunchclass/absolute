27e71ae987c39c96a347546353c9e9d01849fd88
Jaechul Lee <arcyze@gmail.com>
2017-03-30 01:55:34 +0900

    use static message if notification data is not exist (#175)
    This change is for wedding.
    Before applying new implementation getting notification data by user id, use static message.

--- client/sw.js
+++ client/sw.js
@@ -1,17 +1,31 @@
-/* eslint-disable */
-
 self.addEventListener('push', function (event) {
-  var data = {};
+  var title;
+  var body;
+
   if (event.data) {
-    data = event.data.json();
+    console.log('get push notification data');
+    const data = event.data.json();
+    title = data.notification.title;
+    body = data.notification.body;
+  } else {
+    console.log('use static push notification data');
+
+    title = '나동진♡김소영 결혼합니다!';
+    const notificationOptions = {
+      body: '2017년 4월 8일 토요일 오후 5시',
+      icon: './images/main.jpg',
+      badge: './images/badge-72x72.png',
+      tag: 'wedding',
+      url: 'https://nadongguri.com/wedding/',
+    };
+    body = notificationOptions;
   }
 
-  event.waitUntil(
-    self.registration.showNotification(
-      data.notification.title, data.notification)
-  );
+  event.waitUntil(self.registration.showNotification(title, body));
 });
 
 self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+  /* eslint-disable */
+  clients.openWindow('https://nadongguri.com/wedding/');
 });
-
