850369788c79d47a9540ef8063841bfbd877cee6
Jinho Bang <jinho.bang@samsung.com>
2017-04-08 10:54:22 +0900

    Fix SW for wedding demo.

--- client/sw.js
+++ client/sw.js
@@ -1,14 +1,12 @@
 /* eslint-disable */
 importScripts('authorization/authorization.js');
 
-var serverUrl = 'https://nadongguri.com';
-
 var fetchRequest = function (targetUrl, method, data) {
   return new Promise(function (resolve, reject) {
     fetch(targetUrl, { method, body: data }).then(function (response) {
       if (response.status !== 200) {
-        console.log(`Failed to fetch from ${targetUrl} Status Code:
-          ${response.status}`);
+        console.log('Failed to fetch from ${targetUrl} Status Code: '
+            + response.status);
         return reject(response.status);
       }
       response.json().then(function (respData) {
@@ -17,48 +15,47 @@ var fetchRequest = function (targetUrl, method, data) {
       });
     })
     .catch(function (err) {
-      console.log(`Fetch Error : ${err}`);
+      console.log('Fetch Error : ' + err);
       reject(err);
     });
   });
 };
 
-self.addEventListener('push', function (event) {
-  var title;
-  var body;
-  if (event.data) {
-    console.log('use push payload data');
-    const data = event.data.json();
-    event.waitUntil(self.registration.showNotification(
-      data.notification.title, data.notification.body));
+function isWeddingBegun() {
+  const dday = new Date(2017, 3, 8, 16, 30, 0); // month starts from 0
+  const now = new Date();
+  if (now.getTime() >= dday.getTime()) {
+    return true;
   } else {
-    console.log('use server push notification data');
-    event.waitUntil(getUuid().then(function (auth) {
-      fetchRequest(`${serverUrl}/api/push/notification/message/${auth.uuid}`,
-        'GET').then(function (notification) {
-          title = notification.title;
-          body = {
-            body: notification.body,
-            icon: notification.icon,
-            data: {
-              url: notification.url,
-            },
-          };
-          event.waitUntil(self.registration.showNotification(title, body));
-        }).catch(function(error) {
-          console.log(`failed to get server push notification data ${error}`);
-      });
-    }));
+    return false;
   }
+}
+
+self.addEventListener('push', function (event) {
+  var title = "축하해주셔서 감사합니다.";
+  const notificationOptions = {
+    body: "클릭하여 신랑이 준비한 선물을 받아가세요.",
+    icon: "",
+    data: {
+      url: '/coupon'
+    }
+  };
+
+  if (!isWeddingBegun()) {
+    title = "4월8일 토요일 5시";
+    notificationOptions.body = "나동진♡김소영 결혼을 축하해주세요!!!";
+  }
+
+  event.waitUntil(
+      self.registration.showNotification(title, notificationOptions));
 });
 
 self.addEventListener('notificationclick', function (event) {
-  const dday = new Date(2017, 3, 8, 16, 30, 0); // month starts from 0
-  const now = new Date();
-  if( now.getTime() >= dday.getTime() ){
-    event.waitUntil(clients.openWindow(`${serverUrl}/coupon`));
-  } else {
-    event.waitUntil(clients.openWindow(`${serverUrl}/wedding/`));
-  }
   event.notification.close();
+
+  var url = "/coupon";
+  if (!isWeddingBegun()) {
+    url = "/wedding";
+  }
+  event.waitUntil(clients.openWindow(url));
 });
