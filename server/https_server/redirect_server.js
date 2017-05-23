34e55fe22e1e84bff68570ac7544ec9d27c43585
daehyunjimmy <jimmysung201@gmail.com>
2017-05-23 11:16:42 +0900

    Replace require() method with import statement for ES6,7 support (#233)
    We don't want to use require() method anymore in this project.

--- server/https_server/redirect_server.js
+++ server/https_server/redirect_server.js
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-const express = require('express');
-const http = require('http');
+import express from 'express';
+import http from 'http';
 
 /**
  * Redirect http to https
