2bf2620d7d7d8a489979354101ec09aaa544e326
Wuseok <rufia00@gmail.com>
2017-05-28 21:11:11 +0900

    Add to check node, npm, mongodb module

--- test/test-start.js
+++ test/test-start.js
@@ -1,10 +1,31 @@
 var assert = require("assert"); // assert module is supported from nodejs.
+const execSync = require('child_process').execSync;
 
-describe('mocha test start', function(){
-	console.log("mocha test start");
+describe('Module check', function(){
+	it('Is mongo db installed?', function() {
+		execSync('./third_party/mongodb/bin/mongod -version', function(err, stdout, stderr){
+			if(err) assert(false);
+			else assert(true);
+		});
+	});
+
+	it('Is node installed?', function() {
+		execSync('./third_party/node/bin/node -v', function(err, stdout, stderr){
+			if(err) assert(false);
+			else assert(true);
+		});
+	});
+
+	it('Is npm installed?', function() {
+		execSync('./third_party/node/bin/npm -v', function(err, stdout, stderr){
+			if(err) assert(false);
+			else assert(true);
+		});
+	});
 });
 
 
+/*
 describe('Array test', function(){
 	describe('indexOf() method', function(){
 		it('if it is not result, -1 was returned', function() {
@@ -13,3 +34,4 @@ describe('Array test', function(){
 		});
 	});
 });
+*/
