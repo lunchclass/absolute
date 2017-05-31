0d8b19a4826ae0b3fa80b9e39bcfbfff33276ee4
Wuseok <rufia00@gmail.com>
2017-06-01 00:05:21 +0900

    additionally update bootstrap test part

--- bootstrap/test/test-start.js
+++ bootstrap/test/test-start.js
@@ -2,22 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var assert = require('assert');
-const execSync = require('child_process').execSync;
 // FIXME(Wuseok): babel is not build for test module.
 // Current babel was built in server part so It'll fix later.
 // please check github issue number #242
 
+const assert = require('assert');
+const execSync = require('child_process').execSync;
+
 describe('Module check', () => {
-	it('chai Is mongo db installed?', () => {
-		execSync('./third_party/mongodb/bin/mongod -version');
-	});
+  it('Is mongo db installed?', () => {
+	execSync('./third_party/mongodb/bin/mongod -version');
+  });
 
-	it('Is node installed?', () => {
-		execSync('./third_party/node/bin/node -v');
-	});
+  it('Is node installed?', () => {
+	execSync('./third_party/node/bin/node -v');
+  });
 
-	it('Is npm installed?', () => {
-		execSync('./third_party/node/bin/npm -v');
-	});
+  it('Is npm installed?', () => {
+	execSync('./third_party/node/bin/npm -v');
+  });
 });
--- bootstrap/test/test-update.js
+++ bootstrap/test/test-update.js
@@ -2,14 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var assert = require('assert');
-const execSync = require('child_process').execSync;
 // FIXME(Wuseok): babel is not build for test module.
 // Current babel was built in server part so It'll fix later.
 // please check github issue number #242
 
+const assert = require('assert');
+const execSync = require('child_process').execSync;
+
 describe('Update Test', () => {
-	it('Is update test running?', () => {
-		assert(true);
-	});
+  it('Is update was fine?', () => {
+	// FIXME(Wuseok): It will be updated new test module here.
+	// test-update.js will be check about npm update is successfully or not.
+	assert(true);
+  });
 });
