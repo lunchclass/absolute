2ee4f0d0e72fb616266fc57b3b32305d706f08c7
Taeyun Kim <coconutperm@gmail.com>
2017-06-11 20:30:46 +0900

    Database connection (#283)
    Need to connect to database server when starting server.

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import bodyParser from 'body-parser';
 import config from './config';
 import httpsServer from './https_server/https_server';
 import redirectServer from './https_server/redirect_server';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -19,5 +20,6 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
+mongoose.connect(`${config.ip}:${config.dbPort}/absolute`);
 httpsServer.run(app, config.serverInfo);
 redirectServer.runForHttps(config.serverInfo);
