ebfe3546c6f09210b223c2d2fb08b2daa5529408
CoconutPerm <coconutperm@gmail.com>
2017-06-15 21:36:54 +0900

    Migration authentication
    It's migration for uuid generator from hackaton branch.

---
+++ server/authorization/controller/auth_controller.js
@@ -0,0 +1,38 @@
+// Copyright (c) 2017 The Absolute Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.gi
+import Auth from '../model/auth';
+import generator from 'uuid/v4';
+
+export const generateUuid = function generateUuid() {
+  return new Promise((resolve, reject) => {
+    const authInfo = {
+      uuid: generator(),
+    };
+    const auth = new Auth(authInfo);
+    auth.save((error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.stringify(authInfo));
+      }
+    });
+  });
+};
+
+export const getUuid = function getUuid(clientUuid) {
+  return new Promise((resolve, reject) => {
+    const select = {uuid: `${clientUuid}`};
+    Auth.findOne(select, (error, authInfo) => {
+      if (error) {
+        reject(error);
+      } else {
+        if (authInfo === null || authInfo === undefined || authInfo === '') {
+          console.log(authInfo);
+          reject(-1);
+        }
+        resolve(JSON.stringify(authInfo));
+      }
+    });
+  });
+};
