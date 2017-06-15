2900dcdfea1da5bcbdd375e6b4d4228633ed5bfe
Jinho Bang <jinho.bang@samsung.com>
2017-06-16 03:10:31 +0900

    Create auth_controller.js

--- server/authorization/controller/auth_controller.js
+++ server/authorization/controller/auth_controller.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2017 The Absolute Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.gi
+// found in the LICENSE file.
+
 import {Auth} from '../model/auth';
 import generator from 'uuid/v4';
 
