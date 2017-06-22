abe1072d35f663d175592e183bd3ad3b045c10af
Wuseok <rufia00@gmail.com>
2017-06-23 02:45:16 +0900

    Add a bootstrap test to check whether |npm update| is working well (#282)
    When running |absolute| command, we check whether there is .pkg_timestamp file
    and then run |npm update| if the file is not expired. So, we should add a new
    bootstrap test for checking it.

--- bootstrap/test/test-update.js
+++ bootstrap/test/test-update.js
@@ -7,12 +7,40 @@
 // please check github issue number #242
 
 const assert = require('assert');
-const execSync = require('child_process').execSync;
+const path = require('path');
+const fs = require('fs');
+const spawnSync = require('child_process').spawnSync;
 
+// TODO(zino): Should refactor this test codes.
+/**
+*  < test-update scenario >
+*  1. assert .pkg_timestamp exists (If .pkg_timestamp is not exists, run absolute command)
+*  2. assert .pkg_timestamp's modified time is newer than package.json.
+*  3. Keep .pkg_timestamp's modified time to use it in step 5.
+*  4. run absolute command
+*  5. assert .pkg_timestamp's modified time == kept modified time 
+*   (It means that the .pkg_timestamp's modified time is not updated)
+*
+*/
 describe('Update Test', () => {
-  it('Is update was fine?', () => {
-	// FIXME(Wuseok): It will be updated new test module here.
-	// test-update.js will be check about npm update is successfully or not.
-	assert(true);
+
+  if(!fs.existsSync(".pkg_timestamp")){
+	spawnSync(path.resolve('./absolute'));
+  }
+
+  it('Is npm update check file exist?', () => {
+	assert(fs.existsSync(".pkg_timestamp"));
+  });
+
+  var timestamp_before = fs.statSync(".pkg_timestamp");
+  spawnSync(path.resolve('./absolute'));
+  var timestamp_after = fs.statSync(".pkg_timestamp");
+
+  it('Is npm update working fine?', () => {
+	assert(datesEqual(timestamp_after["mtime"], timestamp_before["mtime"]));
   });
 });
+
+function datesEqual(a, b) {
+    return a.getTime() == b.getTime();
+}
