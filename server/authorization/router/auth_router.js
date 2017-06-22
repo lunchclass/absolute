dad294ccb728e0d3a263f7cc9053c5e7cc444aea
Taeyun Kim <coconutperm@gmail.com>
2017-06-22 14:16:57 +0900

    Migrate authentication (#317)
    This change migrates authentication with UUID from hackaton branch to master.

---
+++ server/authorization/router/auth_router.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2017 The Absolute Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import express from 'express';
+import * as auth from '../controller/auth_controller';
+
+const router = express.router();
+
+router.post('/uuid', (request, response) => {
+  auth.generateUuid().then((authInfo) => {
+    response.send(authInfo);
+  }, (error) => {
+    response.sendStatus(400);
+  });
+});
+
+router.get('/uuid', (request, response) => {
+  if (request.query.uuid) {
+    auth.getUuid(request.query.uuid).then((authInfo) => {
+      response.json(authInfo);
+    }, (error) => {
+      response.sendStatus(400);
+    });
+  } else {
+    response.sendStatus(400);
+  }
+});
+
+module.exports = router;
