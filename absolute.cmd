96ae0b3b431869ced6a808f87f08fa63f32f48a4
Jinho Bang <jinho.bang@samsung.com>
2017-06-25 22:22:02 +0900

    Update absolute.cmd

--- absolute.cmd
+++ absolute.cmd
@@ -11,8 +11,6 @@
 :: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 :: See the License for the specific language governing permissions and
 :: limitations under the License.
-:: Use of this source code is governed by a BSD-style license that can be
-:: found in the LICENSE file.
 
 @ECHO OFF
 set ABSOLUTE_PATH=%~dp0
