3c8f75befa1c5db2f64b518287b1759422bb44c9
daehyunjimmy <jimmysung201@gmail.com>
2017-07-06 00:25:13 +0900

    Modify service worker and service worker manager

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,7 +1,12 @@
+
+
 self.addEventListener('install', (event) => {
   event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('push', function(event) {
+  if (!(self.Notification && self.Notification.permission === 'granted')) {
+    return;
+  }
 });
 
