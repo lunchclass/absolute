293ff2d490b834f0aa92640a1395c044fe336a29
CoconutPerm <coconutperm@gmail.com>
2017-07-08 11:37:21 +0900

    More fixed
    Apply new style router

---
+++ server/order/router/product.router.js
@@ -0,0 +1,36 @@
+// Copyright (c) 2017 The Absolute Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Application from '../../base/application.js';
+
+@Application.route('/product')
+export default class ProductRouter {
+  get(request, response) {
+    product.getProductList()
+      .then((productList) => {
+        response.send(productList);
+      });
+  }
+
+  post(request, response) {
+    if (request.body.name && request.body.amount) {
+      product.addProduct(JSON.parse(JSON.stringify(request.body)))
+        .then((product) => {
+          response.send(product);
+        });
+    } else {
+      response.sendStatus(400);
+    }
+  }
+}
