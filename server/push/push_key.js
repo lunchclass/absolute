c010e51e6b573f08ae59ec06eaaf5b21ef417284
Jaechul Lee <arcyze@gmail.com>
2017-07-11 00:21:40 +0900

    remove commited push key

--- server/push/push_key.js
+++ server/push/push_key.js
@@ -1,7 +1,7 @@
 export default {
   'pushServerKey': 'AAAAuU4aFAY:APA91bHL6gDxgTYMHD7q2hTKD_6GwYOcDhPTjiM14vYWwRwAmkl3rijIRnK-9-NdFvy8hrcXvi8IvBLhEPOUQZxZ2N00roSkH1U3tLX7sOGxzsKkTXHp9R2i-GbmYVkK9yYz0RF_wZfP',
   'pushVapidKeys': {
-    'publicKey': 'BIbiSCvmS679hndNYBB2oqmdMcbNwzS7qvgE0M6JHcPJH-4DiwNX6_F9e61xkIAehs06m2GKVvt71la_QumsZkg',
-    'privateKey': 'm8P4dHj2FhGBFzGNGyvwJZh34IZ1_j-Bs0hJH1W2z8s',
+    'publicKey': '',
+    'privateKey': '',
   },
 };
