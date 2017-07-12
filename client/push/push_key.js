808ebad96006b8e8f7f5a5e3e3fbf901911681a3
Jinho Bang <jinho.bang@samsung.com>
2017-07-12 22:47:46 +0900

    haha

--- client/push/push_key.js
+++ client/push/push_key.js
@@ -1,5 +1,5 @@
 export default {
   'pushVapidKeys': {
-    'publicKey': ''
+    'publicKey': 'BDRiyQ9lzaXApdQPHbqqgTRWjf_nFasd9Fm9ghB4bjIsOUYXb8t8NRfhwp_f7aWkHqxKhoOz6lankrhrSiQ-jKk'
   }
 }
--- server/push/push_key.js
+++ server/push/push_key.js
@@ -1,7 +1,7 @@
 export default {
   'pushServerKey': 'AAAAuU4aFAY:APA91bHL6gDxgTYMHD7q2hTKD_6GwYOcDhPTjiM14vYWwRwAmkl3rijIRnK-9-NdFvy8hrcXvi8IvBLhEPOUQZxZ2N00roSkH1U3tLX7sOGxzsKkTXHp9R2i-GbmYVkK9yYz0RF_wZfP',
   'pushVapidKeys': {
-    'publicKey': '',
-    'privateKey': '',
+    'publicKey': 'BDRiyQ9lzaXApdQPHbqqgTRWjf_nFasd9Fm9ghB4bjIsOUYXb8t8NRfhwp_f7aWkHqxKhoOz6lankrhrSiQ-jKk',
+    'privateKey': 'UjbvhaggzZy5WBIEs8O_MYiDVITBAnNSty_4SzbeO68',
   },
 };
