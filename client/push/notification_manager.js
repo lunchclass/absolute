dd3176ad3ebc93d27457f7fadd86f442a16b9f19
daehyunjimmy <jimmysung201@gmail.com>
2017-07-30 18:13:51 +0900

    handle push notification event in service worker

--- client/push/notification_manager.js
+++ client/push/notification_manager.js
@@ -28,13 +28,37 @@ export default class Notification {
   /**
    * create notification
    */
-  createNotification(title, option) {
+  createNotification(event) {
+    let notiPromise;
+    if (event.data) {
+      notiPromise = Promise.resolve(event.data.json());
+    } else {
+      // get data information from server
+    }
+
+    notiPromise = notiPromise.then(data => {
+      return self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: data.icon,
+        tag: data.tag
+      });
+    });
+    event.waitUntil(notiPromise);
   }
 
   /**
    * close notification
    */
-  closeNotification() {
+  closeNotification(event) {
+    event.notification.close();
+  }
+
+  /**
+   * process notification click event
+   */
+  processNotificationClickEvent(event) {
+    let url = '';
+	event.waitUntil(clients.openWindow(url));
   }
 }
 
--- client/service-worker.js
+++ client/service-worker.js
@@ -1,5 +1,7 @@
 import Notification from './push/notification_manager.js';
 
+var notification = new Notification();
+
 self.addEventListener('install', (event) => {
   event.waitUntil(self.skipWaiting());
 });
@@ -8,4 +10,11 @@ self.addEventListener('push', function(event) {
   if (!(self.Notification && self.Notification.permission === 'granted')) {
     return;
   }
+
+  notification.createNotification(event);
+});
+
+self.addEventListener('notificationclick', function (event) {
+  notification.closeNotification(event);
+  notification.processNotificationClickEvent(event);
 });
