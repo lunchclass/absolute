3ab5f2534da46959c8f4499d591e5ac2da585540
Jaechul Lee <arcyze@gmail.com>
2017-08-24 22:38:13 +0900

    Add manifest.json for samsung browser (#415)
    - samsung browser uses manifest.json for push registration
    - If we don't provide manifest.json, we could see below message
      "DOMException: Registration failed - manifest empty or missing"

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -139,7 +139,8 @@ gulp.task('bootstrap_test', () => {
 });
 
 gulp.task('build_client', () => {
-  gulp.src(path.resolve(__dirname, 'client', 'index.html'))
+  gulp.src([path.resolve(__dirname, 'client', 'index.html'),
+    path.resolve(__dirname, 'client', 'manifest.json')])
     .pipe(gulp.dest(path.resolve(__dirname, 'out', 'client')));
   webpack({
     watch: true,
