df7e35a88f8d39d45aba04ba8883a7ef8d5f4277
PeterKim <31546782+sapzape@users.noreply.github.com>
2017-10-07 23:52:15 +0900

    Add path module
    I added path module to work in any environment.
    
    ISSUE=#471

--- bootstrap/test/clean.test.ts
+++ bootstrap/test/clean.test.ts
@@ -1,11 +1,12 @@
 import {} from 'jest';
 import * as childProcess from 'child_process';
 import * as fs from 'fs';
+import * as path from 'path';
 
 test('GET /test', async() => {
-    childProcess.spawnSync('./absolute', ['build_server'], { shell: true });
+    childProcess.spawnSync(path.resolve('./absolute'), ['build_server'], { shell: true });
     expect(fs.existsSync('./out')).toBe(true);
 
-    childProcess.spawnSync('./absolute', ['clean'], { shell: true });
+    childProcess.spawnSync(path.resolve('./absolute'), ['clean'], { shell: true });
     expect(fs.existsSync('./out')).toBe(false);
 });
