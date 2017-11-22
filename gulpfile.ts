ee4c2419f8ff9c5689bbe87964abe7a9624fbef1
changsu lee <sollcs.star@gmail.com>
2017-11-23 02:52:35 +0900

    Apply color on test result log (#538)
    Color is missing after using child_process.exec() function.
    Change it to spawn() function to inherit parent stdio.
    
    ISSUE=none

--- gulpfile.ts
+++ gulpfile.ts
@@ -108,10 +108,10 @@ gulp.task('stop-mongo', runCommand(`${mongodb_path}/bin/mongo --eval "use admin;
 
 function runCommand(command: string) {
   return (cb: any) => {
-    childProcess.exec(command, (error: any, stdout: any, stderr: any) => {
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-        cb(error);
+    childProcess.spawn(command, [], {
+      shell: true,
+      stdio : ['inherit', 'inherit', 'inherit']
     });
+    cb();
   }
 }
